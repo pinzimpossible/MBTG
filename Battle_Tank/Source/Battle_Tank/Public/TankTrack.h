1ab61b9aca084a28c22c0fbb6cfe6fac172af0df
Toai Huynh <pinz.impossible@gmail.com>
2019-07-16 13:48:57 +0700

    Tank_movable

---
+++ Battle_Tank/Source/Battle_Tank/Public/TankTrack.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack set to drive force
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLE_TANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 50000000;
+};
